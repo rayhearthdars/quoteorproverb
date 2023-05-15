c1d2dc6fc43ba161975040eb054e8a37f61c02ea
Julia Hu <40742100+jhukosh@users.noreply.github.com>
2023-05-15 11:16:20 +0200

    feedback favoriteMessages.js

--- js/favoriteMessages.js
+++ js/favoriteMessages.js
@@ -1,5 +1,7 @@
 ///////////////////////////////////////////////////////////////////////
 const returnToIndexPageFromFavorite = document.getElementById("returnToIndexPageFromFavorite");
+// You can have a const
+// const getFavoriteFromLocalStorage = JSON.parse(localStorage.getItem("favoriteMessagesArray"));
 let getFavoriteFromLocalStorage = localStorage.getItem("favoriteMessagesArray");
 getFavoriteFromLocalStorage = JSON.parse(getFavoriteFromLocalStorage);
 const favoriteMessageBoard = document.getElementById("favoriteMessageBoard");
@@ -16,6 +18,8 @@ const returnToHomePage = () => {
 //display messages
 
 const displayMessageOnFavoritePage = () => {
+    // Ok I think it is bette to declare the const in this scope
+    // for (const favorite of getFavoriteFromLocalStorage) {...}
     for (favorite of getFavoriteFromLocalStorage) {
         let newFavorite = document.createElement("p");
         newFavorite.className = "favoriteSentences";
